50e42bf8f82d47c4f49ecfb623427420b99e39b5
Abel <dev@abelasfaw.com>
2026-02-01 21:19:06 -0500

    Round give button

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -35,9 +35,8 @@ export function Navbar() {
           <Button
             asChild
             fontWeight="500"
-            borderRadius="md"
+            rounded="full"
             disabled={isLoading || isError}
-            _hover={{ background: 'text.primary', color: 'white' }}
           >
             <Link
               href={siteSettings?.giveLink}
