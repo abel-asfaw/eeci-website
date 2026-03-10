ca0cc732a3fae7c41f0046632a6a8920d1fc0d5a
Bostesa <natestesa@gmail.com>
2026-03-10 22:47:03 +0100

    Adding Hamburger menu

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,11 +1,29 @@
-import { Box, Flex, HStack, Image, Link } from '@chakra-ui/react';
-import { Link as RouterLink } from 'react-router-dom';
+import { useState } from 'react';
+import {
+  Box,
+  Flex,
+  HStack,
+  IconButton,
+  Image,
+  Link,
+  VStack,
+} from '@chakra-ui/react';
+import { Link as RouterLink, useLocation } from 'react-router-dom';
+import { LuMenu, LuX } from 'react-icons/lu';
 import { useSiteSettings } from '../../hooks/useSiteSettings';
 import { NAVIGATION_LINKS } from '../../constants';
 import { OutlineButton, ExternalLink } from '../ui';
 
 export function Navbar() {
   const { data: siteSettings, isLoading, isError } = useSiteSettings();
+  const [menuOpen, setMenuOpen] = useState(false);
+  const location = useLocation();
+
+  // Close menu on route change
+  const prevPath = location.pathname;
+  if (menuOpen && prevPath !== location.pathname) {
+    setMenuOpen(false);
+  }
 
   return (
     <Box
@@ -34,7 +52,9 @@ export function Navbar() {
             />
           </RouterLink>
         </Link>
-        <HStack as="ul" listStyleType="none" gap="6">
+
+        {/* Desktop links */}
+        <HStack as="ul" listStyleType="none" gap="6" display={{ base: 'none', md: 'flex' }}>
           {NAVIGATION_LINKS.map(({ label, href }) => (
             <Box as="li" key={label}>
               <Link asChild>
@@ -55,7 +75,64 @@ export function Navbar() {
             </OutlineButton>
           </Box>
         </HStack>
+
+        {/* Mobile hamburger */}
+        <IconButton
+          aria-label={menuOpen ? 'Close menu' : 'Open menu'}
+          variant="ghost"
+          size="lg"
+          display={{ base: 'flex', md: 'none' }}
+          onClick={() => setMenuOpen(!menuOpen)}
+        >
+          {menuOpen ? <LuX size={24} /> : <LuMenu size={24} />}
+        </IconButton>
       </Flex>
+
+      {/* Mobile dropdown */}
+      {menuOpen && (
+        <VStack
+          align="stretch"
+          gap="1"
+          paddingX="4"
+          paddingY="4"
+          borderTop="1px solid"
+          borderColor="gray.100"
+          display={{ base: 'flex', md: 'none' }}
+          position="absolute"
+          top="100%"
+          left="0"
+          right="0"
+          background="white"
+          boxShadow="md"
+          zIndex="99"
+        >
+          {NAVIGATION_LINKS.map(({ label, href }) => (
+            <Link
+              key={label}
+              asChild
+              fontSize="lg"
+              fontWeight="500"
+              paddingY="2"
+              onClick={() => setMenuOpen(false)}
+            >
+              <RouterLink to={href}>{label}</RouterLink>
+            </Link>
+          ))}
+          <Box paddingTop="2">
+            <OutlineButton
+              color="white"
+              backgroundColor="gray.900"
+              width="100%"
+              asChild
+              disabled={isLoading || isError}
+            >
+              <ExternalLink href={siteSettings?.giveLink} disabled={isError}>
+                Give
+              </ExternalLink>
+            </OutlineButton>
+          </Box>
+        </VStack>
+      )}
     </Box>
   );
 }
